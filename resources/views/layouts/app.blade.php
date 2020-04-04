2bc6b137ca9f5786098e860b234868b687b02c54
Miguel Angel <m.angel@cityserving.com>
2020-04-04 14:38:59 +0200

    Login terminado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,7 +68,7 @@
                         </li>
 
                         <li class="nav-item text-center">
-                            <a href="#signup" class="btn align-middle btn-outline-primary my-2 my-lg-0">Login</a>
+                            <a href="login" class="btn align-middle btn-outline-primary my-2 my-lg-0">Login</a>
                         </li>
                         <li class="nav-item text-center">
                             <a href="#signup" class="btn align-middle btn-primary my-2 my-lg-0">Sign Up</a>
@@ -90,6 +90,55 @@
     <main id="app">
         @yield('content')
     </main>
+
+    <div class="section bg-light mt-4" id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4"> <img src="images/global/logo-dark.svg" class="logo-dark" alt="iTweetlytics Logo" />
+                    <p class="ml-1">
+                        <a href="https://themeforest.net/user/surjithctly/portfolio?ref=surjithctly&utm_source=footer_content" target="_blank">Purchase now →</a>
+                    </p>
+                    <!-- // end .lead -->
+                </div>
+                <!-- // end .col-sm-3 -->
+                <div class="col-sm-2">
+                    <ul class="list-unstyled footer-links ml-1">
+                        <li><a href="#portfolio">Portfolio</a></li>
+                        <li><a href="#about">About us</a></li>
+                        <li><a href="#services">Services</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+                <!-- // end .col-sm-3 -->
+                <div class="col-sm-2">
+                    <ul class="list-unstyled footer-links ml-1">
+                        <li><a href="#">Terms</a></li>
+                        <li><a href="#about">Privacy</a></li>
+                    </ul>
+                </div>
+                <!-- // end .col-sm-3 -->
+                <div class="col-sm-2">
+                    <ul class="list-unstyled footer-links ml-1">
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Linkedin</a></li>
+                    </ul>
+                </div>
+                <!-- // end .col-sm-3 -->
+                <div class="col-sm-2">
+                    <a href="#home" class="btn btn-sm btn-outline-primary ml-1">Go to Top</a>
+                </div>
+                <!-- // end .col-sm-3 -->
+            </div>
+            <!-- // end .row -->
+            <div class=" text-center mt-4">
+                <small class="text-muted">Copyright © <script type="text/javascript">document.write(new Date().getFullYear());</script> All rights reserved. iTweetlytics.</small>
+            </div>
+        </div>
+        <!-- // end .container -->
+    </div>
+    <!-- // end #about.section -->
+    <!-- // end .agency -->
 
     <!-- JS Files -->
     <!-- build:js js/app.min.js -->
