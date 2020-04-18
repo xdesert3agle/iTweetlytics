396f12f03fb0973402df5f9ebb5b24f4aca732cf
Miguel Angel <m.angel@cityserving.com>
2020-04-18 12:36:47 +0200

    Fetch fix

--- app/Jobs/UpdateFollowersList.php
+++ app/Jobs/UpdateFollowersList.php
@@ -3,7 +3,6 @@
 namespace App\Jobs;
 
 use App\Follower;
-use App\TwitterProfile;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -17,15 +16,15 @@ class UpdateFollowersList implements ShouldQueue {
     const MAX_CONSECUTIVE_REQUESTS = 15;
     const FOLLOWERS_PER_REQUEST = 5000;
 
-    protected $twitterProfile;
+    protected $profile;
 
     /**
      * Create a new job instance.
      *
-     * @return void
+     * @param $profile
      */
-    public function __construct($twitterProfile) {
-        $this->twitterProfile = $twitterProfile;
+    public function __construct($profile) {
+        $this->profile = $profile;
     }
 
     /**
@@ -33,35 +32,32 @@ class UpdateFollowersList implements ShouldQueue {
      *
      * @return void
      */
-    public function handle() {/*
-        $cursor = $this->twitterProfile->next_followers_cursor;
-        $cursor = -1;
-
-        if ($cursor == 0)
-            $cursor = -1;
-
+    public function handle() {
+        $cursor = $this->profile->next_followers_cursor;
         $count = 0;
 
+        // Se reconfigura la API de Twitter con los tokens de acceso del perfil
         Twitter::reconfig([
-            "token" => $this->twitterProfile->oauth_token,
-            "secret" => $this->twitterProfile->oauth_token_secret,
+            "token" => $this->profile->oauth_token,
+            "secret" => $this->profile->oauth_token_secret,
         ]);
 
-        $followers = [];
-
         do {
-            $response = Twitter::getFollowersIds(['screen_name' => $this->twitterProfile->screen_name, 'cursor' => $cursor]);
+            $response = Twitter::getFollowersIds(['screen_name' => $this->profile->screen_name, 'cursor' => $cursor, 'count' => 5000]);
             $cursor = $response->next_cursor;
 
+            // Se inserta una entrada en la tabla de followers por cada ID recogida en la petición
             foreach ($response->ids as $id) {
-                /*$follower = new Follower;
-                $follower->twitter_profiles_id = $this->twitterProfile->id;
+                $follower = new Follower;
+                $follower->twitter_profiles_id = $this->profile->id;
                 $follower->twitter_user_id = $id;
                 $follower->save();
-                $followers[$count][] = $id;
             }
-        } while ($cursor != 0 && ++$count < self::MAX_CONSECUTIVE_REQUESTS);
 
-        dd($followers);*/
+        } while ($cursor != 0 && ++$count < self::MAX_CONSECUTIVE_REQUESTS); // Hasta que el cursor sea 0 o hasta límite de repeticiones
+
+        // Se guarda el cursor si aún no se ha terminado de recorrer todas las páginas. Si no, se pone a -1
+        $this->profile->next_cursor = $cursor != 0 ? $cursor : -1;
+        $this->profile->save();
     }
 }
