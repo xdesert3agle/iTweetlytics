66c03418e40b5d0337d874c93ef8da8e190180a8
Miguel Angel <m.angel@cityserving.com>
2020-04-18 12:36:47 +0200

    Fetch fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,13 +6,12 @@ use App\Jobs\UpdateFollowersList;
 use App\TwitterProfile;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Illuminate\Support\Facades\DB;
-use Thujohn\Twitter\Facades\Twitter;
 
 class Kernel extends ConsoleKernel {
     const REQUEST_WINDOW = 15;
     const MAX_CONSECUTIVE_REQUESTS = 15;
     const FOLLOWERS_PER_REQUEST = 5000;
+
     /**
      * The Artisan commands provided by your application.
      *
@@ -22,35 +21,26 @@ class Kernel extends ConsoleKernel {
         //
     ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param \Illuminate\Console\Scheduling\Schedule $schedule
-     * @return void
-     */
     protected function schedule(Schedule $schedule) {
-        /*$profile = Twitter::getUsersLookup(['screen_name' => 'earcos']);
+        $schedule->call(function () {
+            $this->fetchFollowers();
 
-        // Se calcula en número de peticiones necesarias para poder fetchear la lista completa de followers
-        $neededRequests = $profile[0]->followers_count / (self::MAX_CONSECUTIVE_REQUESTS * self::FOLLOWERS_PER_REQUEST);
+        })->daily();
+    }
 
-        // Se programan los jobs necesarios, con suficiente espacio entre ellos para no llegar al Rate Limit
-        for ($i = 0; $i < ceil($neededRequests); $i++) {
-            UpdateFollowersList::dispatch($profile[0])->delay(now()->addMinutes($i * self::REQUEST_WINDOW));
-        }*/
-
-        /*$allProfiles = TwitterProfile::all();
+    protected function fetchFollowers() {
+        $allProfiles = TwitterProfile::all();
 
         foreach ($allProfiles as $profile) {
 
-            // Se calcula en número de peticiones necesarias para poder fetchear la lista completa de followers
-            $neededRequests = $profile->followers_count / (self::MAX_CONSECUTIVE_REQUESTS * self::FOLLOWERS_PER_REQUEST);
+            // Se calcula el número de peticiones necesarias para poder fetchear la lista completa de followers del perfil
+            $neededJobs = $profile->followers_count / (self::MAX_CONSECUTIVE_REQUESTS * self::FOLLOWERS_PER_REQUEST);
 
-            // Se programan los jobs necesarios, con suficiente espacio entre ellos para no llegar al Rate Limit
-            for ($i = 0; $i < ceil($neededRequests); $i++) {
+            // Se mandan los jobs necesarios, con suficiente espacio entre ellos para no llegar al Rate Limit
+            for ($i = 0; $i < ceil($neededJobs); $i++) {
                 UpdateFollowersList::dispatch($profile)->delay(now()->addMinutes($i * self::REQUEST_WINDOW));
             }
-        }*/
+        }
     }
 
     /**
