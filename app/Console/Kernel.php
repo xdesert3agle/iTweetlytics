c9eb445f312645e2f750365c02dc21180ed2ab6a
xdesert3agle <miguel.vicente@goumh.umh.es>
2020-05-10 11:59:42 +0000

    Fix kernel from server

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,9 +9,6 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\Storage;
 
 class Kernel extends ConsoleKernel {
-    const REQUEST_WINDOW = 15;
-    const MAX_CONSECUTIVE_REQUESTS = 15;
-    const FOLLOWERS_PER_REQUEST = 5000;
 
     /**
      * The Artisan commands provided by your application.
